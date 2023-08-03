7a8b0219ec02611b15e363af1154329a083f1a29
Daniil Mikhnevich <114292729+Furcreator@users.noreply.github.com>
2023-08-03 14:40:36 +0200

    add dateBase

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-const contactsRouter = require("./routes/api/contacts");
+const contactsRouter = require("./routes/api/contactsRoute");
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const envPath =
+  process.env.NODE_ENV === "production"
+    ? "./enviroments/production.env"
+    : "./enviroments/development.env";
 
+dotenv.config({ path: envPath });
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((con) => {
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
-app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+app.all("*", (req, res) => {
+  res.status(404).json({
+    message: "Oops! Resource not found..",
+  });
 });
 
 app.use((err, req, res, next) => {
